ce686a4b432c63154f46791f650635c7ad172632
QuentinBubu <QuentinBubu.dev@gmail.com>
2021-05-07 15:05:36 +0200

    Database engine table create finish v1

--- Simple login with simple fw/App/Database/DatabaseCreateColumn.php
+++ Simple login with simple fw/App/Database/DatabaseCreateColumn.php
@@ -5,35 +5,104 @@ namespace App\Database;
 class DatabaseCreateColumn
 {
 
+    /**
+     * @var string $name Name of column
+     * @var string $type Type of column
+     * @var int|null $size Size of column
+     * @var bool $unsigned If column should have UNSIGNED attribute
+     * @var bool $zerofill If column should have ZEROFILL attribute
+     * @var bool $notNull If column should have NOTNULL attribute
+     * @var bool $auto_increment If column should have AUTO_INCREMENT attribute
+     * @var string|null $defaultValue If column have a default value
+     * @var string|null $comments Comments for column
+     * @var string|null $collate If column should have a collate
+     */
     protected $name;
     protected $type;
+    protected $size;
+    protected $unsigned = false;
+    protected $zerofill = false;
+    protected $notNull = false;
+    protected $auto_increment = false;
+    protected $defaultValue;
+    protected $comments;
+    protected $collate;
     protected static $required = ['name', 'type'];
 
-    public static function createColumn()
+    public $request;
+
+    /**
+     * @return DatabaseCreateColumn
+     */
+    public static function createColumn(): DatabaseCreateColumn
     {
         return new DatabaseCreateColumn;
     }
 
-    public function name(string $name)
+    public function debug(): DatabaseCreateColumn
     {
-        $this->name = $name;
+        var_dump(
+            '<pre>',
+            $this,
+            '</pre>'
+        );
         return $this;
     }
 
-    public function type(string $type)
+    /**
+     * @return DatabaseCreateColumn
+     * @throws DatabaseException
+     */
+    public function __call($name, $arguments): DatabaseCreateColumn
     {
-        $this->type = $type;
-        return $this;
+        if (array_key_exists($name, get_class_vars(get_class($this)))) {
+            if (count($arguments) === 0) {
+                $this->{$name} = true;
+            } elseif (count($arguments) === 1) {
+                $this->{$name} = $arguments[0];
+            } else {
+                $this->{$name} = $arguments;
+            }
+            return $this;
+        } else {
+            throw new DatabaseException('Property not found.');
+        }
     }
 
-    public function build()
+    /**
+     * @return array Return all column query with the new
+     */
+    public function build(): array
     {
         foreach (self::$required as $require) {
             if (is_null($this->{$require})) {
                 throw new DatabaseException('A variable required is null');
             }
         }
+        $request[] = 
+            trim(
+                "`{$this->name}`"
+                    . ' '
+                    . strtoupper($this->type)
+                    . (!is_null($this->size) ? "({$this->size})" : '')
+                    . ($this->unsigned ? ' UNSIGNED' : '')
+                    . ($this->zerofill ? ' ZEROFILL' : '')
+                    . ($this->notNull ? ' NOT NULL' : ' NULL')
+                    . ($this->auto_increment ? ' AUTO_INCREMENT' : '')
+                    . (
+                        !is_null($this->defaultValue)
+                        ? ' DEFAULT'
+                            (
+                                $this->defaultValue[1] === 'string'
+                                ? " '{$this->defaultValue[0]}'"
+                                : " {$this->defaultValue[0]}"
+                            )
+                        : '')
 
-        return "{$this->name}  {$this->type}";
+
+                    . (!is_null($this->comments) ? " COMMENT '{$this->comments}'" : '')
+                    . (!is_null($this->collate) ? " COLLATE '{$this->comments}'" : '')
+                );
+        return $request;
     }
 }
--- Simple login with simple fw/App/Database/DatabaseCreateTable.php
+++ Simple login with simple fw/App/Database/DatabaseCreateTable.php
@@ -5,21 +5,83 @@ namespace App\Database;
 class DatabaseCreateTable extends Database
 {
 
-    private $request;
+    /**
+     * @var string $name Table name
+     * @var array $allColumn Contain all columns request
+     * @var array $allIndex Contain all index request
+     * @var string $collate Collate of table
+     * @var string $engine Engine of table
+     */
+    private $name;
+    private $allColumn = [];
+    private $allIndex = [];
+    private $collate = 'utf8_general_ci';
+    private $engine = 'InnoDB';
+    private static $required = ['name'];
 
     public static function createTable(): DatabaseCreateTable
     {
         return new DatabaseCreateTable;
     }
 
-    public function column(): DatabaseCreateTable
+    public function debug(): DatabaseCreateTable
     {
-        $this->request .= 'yes';
+        var_dump(
+            '<pre>',
+            $this,
+            '</pre>'
+        );
         return $this;
     }
 
-    public function debug()
+    /**
+     * @return DatabaseCreateTable
+     * @throws DatabaseException
+     */
+    public function __call($name, $arguments): DatabaseCreateTable
     {
-        var_dump($this);
+        if (array_key_exists($name, get_class_vars(get_class($this)))) {
+            $this->{$name} = $arguments[0];
+            return $this;
+        } else {
+            throw new DatabaseException('Property not found.');
+        }
+    }
+
+    public function column($arguments): DatabaseCreateTable
+    {
+        $this->allColumn[] = $arguments[0];
+        return $this;
+    }
+
+    public function addIndex($arguments)
+    {
+        $this->allIndex[] = 
+            strtoupper($arguments['type'])
+            . ' INDEX '
+            . "`{$arguments['name']}`"
+            . ' (`'
+            . implode('`,`', $arguments['column'])
+            . '`)';
+        return $this;
+    }
+
+    public function build()
+    {
+        foreach (self::$required as $require) {
+            if (is_null($this->{$require})) {
+                throw new DatabaseException('A variable required is null');
+            }
+        }
+
+        $request =
+            "CREATE TABLE `{$this->name}` ("
+            . implode(',', $this->allColumn)
+            . (!is_null($this->allIndex) ? ',' . implode(',', $this->allIndex) : '')
+            . ')'
+            . " COLLATE='{$this->collate}'"
+            . " ENGINE={$this->engine}";
+
+        Database::request($request, [], '');
     }
 }
