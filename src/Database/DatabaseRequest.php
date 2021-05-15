9801646306758e5dde504f7d2483984e5066d74d
QuentinBubu <QuentinBubu.dev@gmail.com>
2021-05-15 23:10:06 +0200

    Add bindParam for specify type

--- src/Database/DatabaseRequest.php
+++ src/Database/DatabaseRequest.php
@@ -11,18 +11,52 @@ class DatabaseRequest
      * @param string $request
      * @param array $values
      * @param string|null $type
-     * 
-     * @return array|void
+     * @param int $mode
+     * @return array|bool
      */
-    public static function request(string $request, array $values, ?string $type)
+    public static function request(string $request, array $values, ?string $type = null, int $mode = PDO::FETCH_ASSOC): array
     {
         try {
             $request = Database::setPDO()->prepare($request);
-            $request->execute($values);
+            $i = 1;
+            foreach ($values as $key => $value) {
+
+                if ($key === '?') {
+                    $key = $i;
+                } else {
+                    $key = ':' . ltrim($key, ':');
+                }
+
+                switch (gettype($value)) {
+                    case 'integer':
+                        $request->bindParam($key, $value, PDO::PARAM_INT);
+                        break;
+                    
+                    case 'string':
+                        $request->bindParam($key, $value, PDO::PARAM_STR);
+                        break;
+
+                    case 'boolean':
+                        $request->bindParam($key, $value, PDO::PARAM_BOOL);
+                        break;
+
+                    case 'NULL':
+                        $request->bindParam($key, $value, PDO::PARAM_NULL);
+                        break;
+
+                    default:
+                        $request->bindParam($key, $value);
+                        break;
+                }
+                $i++;
+            }
+            $request->execute();
             if ($type === 'fetchAll') {
-                return $request->fetchAll(PDO::FETCH_ASSOC);
+                return $request->fetchAll($mode);
             } elseif ($type === 'fetch') {
-                return $request->fetch();
+                return $request->fetch($mode);
+            } else {
+                return true;
             }
         } catch (Exception $e) {
             die('Erreur: ' . $e->getMessage());
